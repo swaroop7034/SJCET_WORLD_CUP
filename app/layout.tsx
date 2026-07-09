1c11d2a1e5e866969a1d82cc09e1e62316bd04dd
Shaun MR <shaunmr009@gmail.com>
2026-07-09 21:07:58 +0530

    hide match fixture

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Anton&family=Inter:wght@400;500;600;700;800&display=swap"
         />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+       
       </head>
       <body>
         <Providers>{children}</Providers>
